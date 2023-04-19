e2f5af48322777344cc52af46bbbc37f99461091
Jynel Kealon <jkealon13@r2happrentice.org>
2023-04-18 21:14:33 -0400

    New Changes

---
+++ Easy/easy.js
@@ -0,0 +1,4 @@
+let num1 = 6; 
+let num2 = 4;
+let difference = num1 - num2
+console.log("The name Thomas is longer than John by", difference, "characters");
---
+++ Hard/hard.js
@@ -0,0 +1,27 @@
+// Below is the add function //
+let add = addFunction(5, 3);
+function addFunction(a, b) {
+  return  a + b;
+}
+console.log("add:", add)
+
+// Below is the subtract function //
+let subtract =  subtractFunction(5, 3);
+function subtractFunction(a, b) {
+   return a - b;
+}
+console.log("subtract:", subtract)
+
+// Below is the multiply function //
+let multiply =  multiplyFunction(5, 3);
+function multiplyFunction(a, b) {
+  return  a * b;
+}
+console.log("multiply", multiply)
+
+// Below is the divide function //
+let divide =  divideFunction(5, 3);
+function divideFunction(a, b) {
+  return  a / b;
+}
+console.log("divide", divide);
